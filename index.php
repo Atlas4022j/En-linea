c857bbe8f797dc2ba52522c4c9484c6eab41821c
atlas4022j <atlas4022j@gmail.com>
2024-10-02 17:50:57 -0500

    En linea

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="resources/css/main.css">
+    <title>Re utilizando codigo</title>
+</head>
+<body class="px-3">
+    <!-- Encabezado-->
+    <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+            <h1 class="text-center ">EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right pr-4">SOLUCIONES MUNDIALES</p>
+        </div>
+    </div>
+    <!-- IMG-->
+    <div class="row">
+        <div class="col-sm-12">
+            <img class="w-100" style="height: 400px;" src="resources/img/baner01.PNG"  alt=""> 
+        </div>
+    </div>
+    <!-- container-->
+     <div class="container mt-5">
+        <h1>SOLUCIONES CREATIVAS</h1>
+        <p>Sin importar cual sea tu negocio, nuestro equipo puedde hacer todo desde cero</p>
+            <div class="row">
+                <div class="card col-sm-12 col-md-4 border-0">
+                <img src="resources/img/idea_01.PNG" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3><i class="bi bi-pencil"></i> DISEÑO</h3>
+                            <p class="card-text">Creamos diseños segun tus necesidades</p>
+                        </div>
+                </div>
+                <div class="card col-sm-12 col-md-4 border-0">
+                    <img src="resources/img/idea_02.PNG" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3><i class="bi bi-flower1"></i> DESARROLLO</h3>
+                            <p class="card-text">Nuestros programadores te garantizaran la perfeccion </p>
+                        </div>
+                </div>
+                <div class="card col-sm-12 col-md-4 border-0">
+                    <img src="resources/img/idea_03.PNG" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3><i class="bi bi-plus-square"></i> EDICION DE LIBROS</h3>
+                            <p class="card-text">Que el producto final llegue a tus clientes</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+     </div>
+    <!--Footer-->
+     <footer class="bg-wine mt-3">
+            <p class=" p-3 text-center text-white">Edificio Las Hadas, Guadalajara www.sitioincreible.mx | (33) 3214 6547</p>
+    </footer>
+        
+</body>
+</html>
